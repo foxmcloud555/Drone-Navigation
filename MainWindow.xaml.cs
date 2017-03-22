df581dbb641632ec7024bca1e895ece047285541
jacob clark <jacob.clark1@btinternet.com>
2017-03-22 23:06:50 +0000

    switched to use a multi source frame, can now access depth frame whilst handling the colour frame.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -35,12 +35,14 @@ namespace Microsoft.Samples.Kinect.DepthBasics
         /// <summary>
         /// Readers for sensor frames
         /// </summary>
-        private DepthFrameReader m_depthFrameReader = null;
-        private ColorFrameReader m_rgbFrameReader = null;
+        private MultiSourceFrameReader m_FrameReader = null;
         /*
-            Originally we were going to go with infrared due to it not being affected by light 
-            in the same way as colour is, however the object colour is what we are filtering 
-            by to track the objects as it is the more intuitive solution.
+            Opted for multi source frame reader to allow us to process depth and colour at the
+            same time rather than seperatly as it simplifies the issue greatly.
+
+            Originally we were going to go with infrared over colour due to it not being affected 
+            by light in the same way as colour is, however the object colour is what we are 
+            filtering by to track the objects as it is the more intuitive solution for our needs.
         */
         
         // Colour bitmap to be recieved by the kinect
@@ -61,17 +63,15 @@ namespace Microsoft.Samples.Kinect.DepthBasics
         public MainWindow()
         {
             // get the kinectSensor object and open the frame readers
-            this.m_kinectSensor = KinectSensor.GetDefault();
-            this.m_depthFrameReader = this.m_kinectSensor.DepthFrameSource.OpenReader();
-            this.m_rgbFrameReader = this.m_kinectSensor.ColorFrameSource.OpenReader();
+            m_kinectSensor = KinectSensor.GetDefault();
+            m_FrameReader = m_kinectSensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth);
 
             // wire handler for frame arrival
-            this.m_depthFrameReader.FrameArrived += this.Reader_DepthFrameArrived;
-            this.m_rgbFrameReader.FrameArrived += this.Reader_ColourFrameArrived;
+            m_FrameReader.MultiSourceFrameArrived += this.Reader_FrameArrived;
 
             // set up the FrameDescriptions
-            this.m_depthFrameDescription = this.m_kinectSensor.DepthFrameSource.FrameDescription;
-            this.m_colourFrameDescription = this.m_kinectSensor.ColorFrameSource.FrameDescription;
+            m_depthFrameDescription = this.m_kinectSensor.DepthFrameSource.FrameDescription;
+            m_colourFrameDescription = this.m_kinectSensor.ColorFrameSource.FrameDescription;
 
             // create the bitmap
             this.m_colourBitmap = new WriteableBitmap(this.m_colourFrameDescription.Width, this.m_colourFrameDescription.Height, 96.0, 96.0, PixelFormats.Bgr32, null);
@@ -88,22 +88,10 @@ namespace Microsoft.Samples.Kinect.DepthBasics
             this.InitializeComponent();
         }
 
-        /// <summary>
-        /// INotifyPropertyChangedPropertyChanged event to allow window controls to bind to changeable data
-        /// </summary>
+        // INotifyPropertyChangedPropertyChanged event to allow window controls to bind to changeable data
         public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Gets the bitmap to display
-        /// </summary>
- //       public ImageSource ImageSource
-//        {
- //           get { return this.m_colourBitmap; }
- //       }
-
-        /// <summary>
-        /// Gets or sets the current status text to display
-        /// </summary>
+        
+        // Gets or sets the current status text to display
         public string StatusText
         {
             get { return this.m_statusText; }
@@ -122,23 +110,14 @@ namespace Microsoft.Samples.Kinect.DepthBasics
                 }
             }
         }
-
-        /// <summary>
-        /// Execute shutdown tasks
-        /// </summary>
-        /// <param name="sender">object sending the event</param>
-        /// <param name="e">event arguments</param>
+        
+        // Execute shutdown tasks
         private void MainWindow_Closing(object sender, CancelEventArgs e)
         {
-            if (this.m_depthFrameReader != null)
+            if (m_FrameReader != null)
             {
-                this.m_depthFrameReader.Dispose();
-                this.m_depthFrameReader = null;
-            }
-            if (this.m_rgbFrameReader != null)
-            {
-                this.m_rgbFrameReader.Dispose();
-                this.m_rgbFrameReader = null;
+                m_FrameReader.Dispose();
+                m_FrameReader = null;
             }
             if (this.m_kinectSensor != null)
             {
@@ -147,88 +126,56 @@ namespace Microsoft.Samples.Kinect.DepthBasics
             }
         }
 
-        /// <summary>
-        /// Handles the depth frame data arriving from the sensor
-        /// </summary>
-        /// <param name="sender">object sending the event</param>
-        /// <param name="e">event arguments</param>
-        private void Reader_DepthFrameArrived(object sender, DepthFrameArrivedEventArgs e)
+        // Handles the colour frame data arriving from the sensor
+        private void Reader_FrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
         {
-            using (DepthFrame depthFrame = e.FrameReference.AcquireFrame())
+            // cannot use using with multisource frame
+            MultiSourceFrame multiSourceFrame = e.FrameReference.AcquireFrame();
+            if (multiSourceFrame != null)
             {
-                if (depthFrame != null)
+                using (ColorFrame colorFrame = multiSourceFrame.ColorFrameReference.AcquireFrame())
                 {
-                    // the fastest way to process the body index data is to directly access 
-                    // the underlying buffer
-                    using (Microsoft.Kinect.KinectBuffer depthBuffer = depthFrame.LockImageBuffer())
+                    if (colorFrame != null)
                     {
-                        // verify data
-                        if (((this.m_depthFrameDescription.Width * this.m_depthFrameDescription.Height) == (depthBuffer.Size / this.m_depthFrameDescription.BytesPerPixel)) &&
-                            (this.m_depthFrameDescription.Width == this.m_colourBitmap.PixelWidth) && (this.m_depthFrameDescription.Height == this.m_colourBitmap.PixelHeight))
+                        // Process the colour frame to get an rgba and hsv image for us to use
+                        Image<Rgba, Byte> rgbImage = null;
+                        Image<Hsv, Byte> hsvImage = null;
+                        ProcessColourFrame(colorFrame, ref rgbImage, ref hsvImage);
+
+                        // Filter the image to the calibration values of the Target Object
+                        Mat thresholdMat = FilterHsvImage(hsvImage,
+                            new Hsv(THueMinSlider.Value, TSatMinSlider.Value, TValMinSlider.Value),
+                            new Hsv(THueMaxSlider.Value, TSatMaxSlider.Value, TValMaxSlider.Value));
+
+                        // No need to process tracking during calibration...
+                        if (ViewTypeComboBox.SelectedIndex == 0)
                         {
-                            // Note: In order to see the full range of depth (including the less reliable far field depth)
-                            // we are setting maxDepth to the extreme potential depth threshold
-                            ushort maxDepth = ushort.MaxValue;
+                            // coordinates of largest contour
+                            int x = 0;
+                            int y = 0;
+                            // Track the target object retrieving the x and y coordinates
+                            bool objectFound = TrackObjectOnImage(thresholdMat, ref x, ref y);
+                            // found something then output it...
+                            if (objectFound == true)
+                            {
+                                CvInvoke.Circle(rgbImage, new System.Drawing.Point(x, y), 20, new MCvScalar(0, 0, 255), 2);
+                                CvInvoke.PutText(rgbImage, "Tracking Target", new System.Drawing.Point(x, y + 40), FontFace.HersheySimplex, 1, new MCvScalar(0, 0, 255), 2);
+                                CvInvoke.PutText(rgbImage, x + "," + y, new System.Drawing.Point(x, y + 30), FontFace.HersheySimplex, 1, new MCvScalar(0, 0, 255), 2);
+                            }
 
-                            // If you wish to filter by reliable depth distance, uncomment the following line:
-                            maxDepth = depthFrame.DepthMaxReliableDistance;
-
-                            // process depth...
+                            CvInvoke.Imshow("Output", rgbImage);
+                        }
+                        else if (ViewTypeComboBox.SelectedIndex == 1)
+                        {
+                            CvInvoke.Imshow("Output", thresholdMat);
+                        }
+                        else
+                        {
+                            CvInvoke.Imshow("Output", thresholdMat);
                         }
                     }
                 }
-            }
-        }
-
-        /// <summary>
-        /// Handles the colour frame data arriving from the sensor
-        /// </summary>
-        /// <param name="sender">object sending the event</param>
-        /// <param name="e">event arguments</param>
-        private void Reader_ColourFrameArrived(object sender, ColorFrameArrivedEventArgs e)
-        {
-            using (ColorFrame colorFrame = e.FrameReference.AcquireFrame())
-            {
-                if (colorFrame != null)
-                {
-                    // Process the colour frame to get an rgba and hsv image for us to use
-                    Image<Rgba, Byte> rgbImage = null;
-                    Image<Hsv, Byte> hsvImage = null;
-                    ProcessColourFrame(colorFrame, ref rgbImage, ref hsvImage);
-                    
-                    // Filter the image to the calibration values of the Target Object
-                    Mat thresholdMat = FilterHsvImage(hsvImage,
-                        new Hsv(THueMinSlider.Value, TSatMinSlider.Value, TValMinSlider.Value),
-                        new Hsv(THueMaxSlider.Value, TSatMaxSlider.Value, TValMaxSlider.Value));
-
-                    // No need to process tracking during calibration...
-                    if (ViewTypeComboBox.SelectedIndex == 0)
-                    {
-                        // coordinates of largest contour
-                        int x = 0;
-                        int y = 0;
-                        // Track the target object retrieving the x and y coordinates
-                        bool objectFound = TrackObjectOnImage(thresholdMat, ref x, ref y);
-                        // found something then output it...
-                        if (objectFound == true)
-                        {
-                            CvInvoke.Circle(rgbImage, new System.Drawing.Point(x, y), 20, new MCvScalar(0, 0, 255), 2);
-                            CvInvoke.PutText(rgbImage, "Tracking Target", new System.Drawing.Point(x, y + 40), FontFace.HersheySimplex, 1, new MCvScalar(0, 0, 255), 2);
-                            CvInvoke.PutText(rgbImage, x + "," + y, new System.Drawing.Point(x, y + 30), FontFace.HersheySimplex, 1, new MCvScalar(0, 0, 255), 2);
-                        }
-
-                        CvInvoke.Imshow("Output", rgbImage);
-                    }
-                    else if (ViewTypeComboBox.SelectedIndex == 1)
-                    {
-                        CvInvoke.Imshow("Output", thresholdMat);
-                    }
-                    else
-                    {
-                        CvInvoke.Imshow("Output", thresholdMat);
-                    }
-                }
-            }
+            }   // end of multi-source frame.
         }
 
         // Process the provided colour frame and populate the given rgba and hsv images for us to use
